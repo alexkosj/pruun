93dd829ec093eda3cca9e84eb542f49fff6d72d3
Alex Ko <alexkosj@gmail.com>
2020-07-18 01:13:00 -0700

    try removing isolated fs

--- tests/test_pruun.py
+++ tests/test_pruun.py
@@ -13,36 +13,36 @@ def test_deployment_package():
     runner = CliRunner()
     lambda_file_name = "handler.py"
     package_name = "deployment_package.zip"
-    with runner.isolated_filesystem():
-        with open(lambda_file_name, "w") as f:  # create dummy lambda handler file
-            pass
 
-        result = runner.invoke(pruun, ["package", lambda_file_name])
+    with open(lambda_file_name, "w") as f:  # create dummy lambda handler file
+        pass
 
-        # check for no exceptions
-        assert result.exit_code == 0
+    result = runner.invoke(pruun, ["package", lambda_file_name])
 
-        # check for existence of .zip file
-        assert os.path.isfile(package_name) == True
+    # check for no exceptions
+    assert result.exit_code == 0
 
-        # verify integrity of .zip
-        depens = get_dependency_names()
-        for depen in depens:
-            underscored_name = depen.replace("-", "_").strip()
-            cmd = f"unzip -l {package_name} {underscored_name}*"
-            subprocess.run(
-                cmd,
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.STDOUT,
-                shell=True,
-                check=True,
-            )  # check .zip for all installed packages
+    # check for existence of .zip file
+    assert os.path.isfile(package_name) == True
 
-        cmd = f"unzip -l {package_name} {lambda_file_name}*"
+    # verify integrity of .zip
+    depens = get_dependency_names()
+    for depen in depens:
+        underscored_name = depen.replace("-", "_").strip()
+        cmd = f"unzip -l {package_name} {underscored_name}*"
         subprocess.run(
             cmd,
             stdout=subprocess.DEVNULL,
             stderr=subprocess.STDOUT,
             shell=True,
             check=True,
-        )  # check .zip for lambda handler file
+        )  # check .zip for all installed packages
+
+    cmd = f"unzip -l {package_name} {lambda_file_name}*"
+    subprocess.run(
+        cmd,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.STDOUT,
+        shell=True,
+        check=True,
+    )  # check .zip for lambda handler file
