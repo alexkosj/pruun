57f366b8b0cb51c47feb8756dfb427e36b2f85d0
Alex Ko <alexkosj@gmail.com>
2020-07-26 17:41:43 -0700

    finish test

--- tests/test_pruun.py
+++ tests/test_pruun.py
@@ -56,13 +56,16 @@ def test_lambda_directory_support():
     Similar to test_deployment_package_creation(), except a directory path is passed as the value for `handler_path`.
     """
     runner = CliRunner()
+    lambda_file_name = "handler.py"
+    package_name = "deployment_package.zip"
+
     with runner.isolated_filesystem():
 
         # create dummy dir to containg lambda application code
         lambda_dir_path = os.path.join(os.getcwd(), "lambda_dir")
         os.mkdir(lambda_dir_path)
 
-        lambda_file_path = os.path.join(lambda_dir_path, "handler.py")
+        lambda_file_path = os.path.join(lambda_dir_path, lambda_file_name)
         with open(lambda_file_path, "w") as f:  # create dummy lambda handler file
             pass
 
@@ -72,13 +75,13 @@ def test_lambda_directory_support():
             assert result.exit_code == 0
 
             # check for existence of .zip file
-            assert os.path.isfile("deployment_package.zip") == True
+            assert os.path.isfile(package_name) == True
 
             # verify integrity of .zip
             depens = get_dependency_names()
             for depen in depens:
                 underscored_name = depen.replace("-", "_").strip()
-                cmd = f"unzip -l deployment_package.zip {underscored_name}*"
+                cmd = f"unzip -l {package_name} {underscored_name}*"
                 subprocess.run(
                     cmd,
                     stdout=subprocess.DEVNULL,
@@ -86,5 +89,14 @@ def test_lambda_directory_support():
                     shell=True,
                     check=True,
                 )
+
+            cmd = f"unzip -l {package_name} {lambda_file_name}*"
+            subprocess.run(
+                cmd,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.STDOUT,
+                shell=True,
+                check=True,
+            )  # check .zip for lambda handler file
 
             f.close()
