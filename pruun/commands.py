bc68b7cce0039425626d55f54d1c2958d8d45ec2
alexkosj <alexkosj@gmail.com>
2020-10-10 18:19:39 -0700

    update help text and readme

--- pruun/commands.py
+++ pruun/commands.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import click
 
 from .utils import create_deployment_package, get_dependency_dirs, get_dependency_names
@@ -18,17 +20,17 @@ def pruun():
     "--package-file",
     type=click.Path(),
     default="deployment_package.zip",
-    help="Desired filename of deployment .zip file.",
+    help="Desired filepath of deployment .zip file.",
 )
 def package(handler_paths, package_file):
     """
-    Creates deployment package .zip file in current directory.
+    Creates deployment package .zip file at given path.
 
-    handler_paths: Name(s) of Lambda handler file(s)/dir(s).
+    handler_paths (Tuple[str, ...]): Name(s) of Lambda handler file(s)/dir(s).
     Trailing slash for a directory path is optional.
 
-    package-file (optional): Desired filename of deployment .zip file.
-    Defaults to "deployment_package.zip".
+    package-file (str, optional): Desired filepath of deployment .zip file.
+    Defaults to "deployment_package.zip" (in the cwd).
     """
     depen_names = get_dependency_names()
     depen_dirs = get_dependency_dirs(depen_names)
